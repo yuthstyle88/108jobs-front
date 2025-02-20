0e9b9d4b7d27f0b023aafcbd06bc65fbe5abf219
Minh Nguyễn <87883380+tonminhce@users.noreply.github.com>
2025-02-20 23:13:38 +0700

    add login page

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Kanit } from "next/font/google";
+import FontAwesomeConfig from "../fontawesome";
+
+const kanit = Kanit({
+  subsets: ["latin", "vietnamese", "thai"],
+  weight: ["400", "500", "600"],
+  style: ["normal", "italic"],
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: "Fastlance.vn",
+    template: "%s | fastlance.vn",
+  },
+  description: "Tổng hợp freelancer chất lượng hàng đầu cho doanh nghiệp",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <FontAwesomeConfig />
+      </head>
+      <body
+        suppressHydrationWarning
+        className={`${kanit.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
--- src/constants/images.ts
+++ src/constants/images.ts
@@ -29,7 +29,8 @@ import google from "../assets/images/categories-image/google-ads.webp";
 import marketing from "../assets/images/categories-image/marketing.webp";
 import promote from "../assets/images/categories-image/promote-page.webp";
 import wordpress from "../assets/images/categories-image/wordpress.webp";
-
+import logodefault from "../assets/images/categories-image/logo-default.svg";
+import conceptbanner from "../assets/images/categories-image/concept-banner-th.svg";
 
 export const CategoriesImage = {
   seo_image,
@@ -42,6 +43,8 @@ export const CategoriesImage = {
   marketing,
   promote,
   wordpress,
+  logodefault,
+  conceptbanner,
 };
 
 export const MegaMenuImage = {
