553a2b3faf91d5ca7f226557cd316a42d1c76150
Minh Nguyễn <87883380+tonminhce@users.noreply.github.com>
2025-02-25 23:41:32 +0700

    update page earn

---
+++ src/app/reward/layout.tsx
@@ -0,0 +1,23 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import SubMenu from "@/components/SubMenu";
+import { ReactNode } from "react";
+
+interface RewardLayoutProps {
+  children: ReactNode;
+}
+
+export default function RewardLayout({ children }: RewardLayoutProps) {
+  return (
+    <>
+
+      <section className="pt-[4.5rem] bg-white">
+        {children}
+      </section>
+    </>
+  );
+}
+
+export const metadata = {
+  title: "Fastwork - reward ",
+};
