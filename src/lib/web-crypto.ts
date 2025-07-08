1b2957f26a655147a34eb9504c1429718d421251
yongyutjantaboot <yuth_style88@hotmail.com>
2025-07-08 20:53:02 +0700

    ีupdate crypto

--- src/lib/web-crypto.ts
+++ src/lib/web-crypto.ts
@@ -1,45 +1,48 @@
+import { webcrypto } from 'node:crypto';
+const crypto = webcrypto;
+
 export async function generateKey(): Promise<CryptoKey> {
-    return await window.crypto.subtle.generateKey(
+    return await crypto.subtle.generateKey(
         {
             name: "AES-CBC",
-            length: 256, // AES-256
+            length: 256,
         },
-        true, // extractable
+        true,
         ["encrypt", "decrypt"]
     );
 }
 
 export async function exportKey(key: CryptoKey): Promise<string> {
-    const rawKey = await window.crypto.subtle.exportKey("raw", key);
+    const rawKey = await crypto.subtle.exportKey("raw", key);
     return Array.from(new Uint8Array(rawKey)).map(b => b.toString(16).padStart(2, '0')).join('');
 }
 
 export async function importKey(hexKey: string): Promise<CryptoKey> {
     const raw = new Uint8Array(hexKey.match(/.{1,2}/g)!.map(byte => parseInt(byte, 16)));
-    return await window.crypto.subtle.importKey("raw", raw, { name: "AES-CBC" }, false, ["encrypt", "decrypt"]);
+    return await crypto.subtle.importKey("raw", raw, { name: "AES-CBC" }, false, ["encrypt", "decrypt"]);
 }
 
 export async function encrypt(data: string, key: CryptoKey, sessionId: string): Promise<{ ciphertext: string, iv: string }> {
     const iv = new TextEncoder().encode(sessionId.padEnd(21, '0').slice(5, 21)).slice(0, 16);
     const encoded = new TextEncoder().encode(data);
 
-    const ciphertextBuffer = await window.crypto.subtle.encrypt(
+    const ciphertextBuffer = await crypto.subtle.encrypt(
         { name: "AES-CBC", iv },
         key,
         encoded
     );
 
     return {
-        ciphertext: btoa(String.fromCharCode(...new Uint8Array(ciphertextBuffer))),
-        iv: btoa(String.fromCharCode(...iv))
+        ciphertext: Buffer.from(ciphertextBuffer).toString('base64'),
+        iv: Buffer.from(iv).toString('base64')
     };
 }
 
 export async function decrypt(ciphertextBase64: string, sessionId: string, key: CryptoKey): Promise<string> {
-    const ciphertext = Uint8Array.from(atob(ciphertextBase64), c => c.charCodeAt(0));
+    const ciphertext = Buffer.from(ciphertextBase64, 'base64');
     const iv = new TextEncoder().encode(sessionId.padEnd(21, '0').slice(5, 21)).slice(0, 16);
 
-    const decryptedBuffer = await window.crypto.subtle.decrypt(
+    const decryptedBuffer = await crypto.subtle.decrypt(
         { name: "AES-CBC", iv },
         key,
         ciphertext
