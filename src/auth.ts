1bd0f10ec675cfa9da9eab1c0ed91a24374e3891
yongyutjantaboot <yuth_style88@hotmail.com>
2025-07-10 19:20:08 +0700

    add fix email empty

--- src/auth.ts
+++ src/auth.ts
@@ -87,7 +87,7 @@ export const { handlers, auth, signIn} = NextAuth({
                 Object.assign(token, {
                     accessToken: (user as any).token,
                     roles: user.roles,
-                    email: user.email,
+                    email: user.email ?? "",
                     session: user.session,
                 });
 
