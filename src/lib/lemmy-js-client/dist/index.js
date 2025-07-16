d348d48a05fb71edf54029cbd8b234afa3f4a4e4
yongyutjantaboot <yuth_style88@hotmail.com>
2025-07-16 16:11:49 +0700

    add Lemmy to fast Job change var to camelcase

--- src/lib/lemmy-js-client/dist/index.js
+++ src/lib/lemmy-js-client/dist/index.js
@@ -1,18 +1,17 @@
 "use strict";
-var Createbinding = (this && this.Createbinding) || (Object.create ? (function(o, m, k, k2) {
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.Esmodule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+        desc = { enumerable: true, get: function() { return m[k]; } };
     }
     Object.defineProperty(o, k2, desc);
 }) : (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
 }));
-var Exportstar = (this && this.Exportstar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) Createbinding(exports, m, p);
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
-Object.defineProperty(exports, "Esmodule", { value: true });
-// index.ts
-export * from "./http";
+Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("./http"), exports);
--- src/lib/lemmy-js-client/dist/other_types.js
+++ src/lib/lemmy-js-client/dist/other_types.js
@@ -1,4 +1,4 @@
 "use strict";
-Object.defineProperty(exports, "Esmodule", { value: true });
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.VERSION = void 0;
 exports.VERSION = "v4";
