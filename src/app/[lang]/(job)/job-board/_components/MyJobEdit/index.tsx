eb803d9b2512d19e7aa3a8b13698c76f0fae85e4
koey <thekhoi.k@gmail.com>
2025-07-30 15:03:45 +0700

    edit rendering image profile header

--- src/app/[lang]/(job)/job-board/_components/MyJobEdit/index.tsx
+++ src/app/[lang]/(job)/job-board/_components/MyJobEdit/index.tsx
@@ -548,20 +548,20 @@ const MyJobEdit = ({jobId}: Props) => {
                         </div>
 
                         {/* Anonymous Post */}
-                        <div className="mb-6 flex items-center">
-                            <label className="relative inline-flex items-center cursor-pointer">
-                                <input
-                                    type="checkbox"
-                                    className="sr-only peer"
-                                    {...register("isAnonymousPost")}
-                                />
-                                <div
-                                    className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
-                                <span className="ml-3 text-gray-700">
-                    {createJobLanguage?.anonymousPostLabel}
-                  </span>
-                            </label>
-                        </div>
+                  {/*      <div className="mb-6 flex items-center">*/}
+                  {/*          <label className="relative inline-flex items-center cursor-pointer">*/}
+                  {/*              <input*/}
+                  {/*                  type="checkbox"*/}
+                  {/*                  className="sr-only peer"*/}
+                  {/*                  {...register("isAnonymousPost")}*/}
+                  {/*              />*/}
+                  {/*              <div*/}
+                  {/*                  className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>*/}
+                  {/*              <span className="ml-3 text-gray-700">*/}
+                  {/*  {createJobLanguage?.anonymousPostLabel}*/}
+                  {/*</span>*/}
+                  {/*          </label>*/}
+                  {/*      </div>*/}
 
                         {/* Buttons */}
                         <div className="flex justify-end space-x-4 mt-10">
