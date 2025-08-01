d5bb0ecde554c8c385c8767fae83d5f5432d92bf
koey <thekhoi.k@gmail.com>
2025-08-01 15:28:47 +0700

    add field to GetPost query and add setup lemmy to script

--- src/lib/lemmy-js-client/src/types/GetPosts.ts
+++ src/lib/lemmy-js-client/src/types/GetPosts.ts
@@ -3,43 +3,31 @@ import type {CommunityId} from "./CommunityId";
 import type {ListingType} from "./ListingType";
 import type {PaginationCursor} from "./PaginationCursor";
 import type {PostSortType} from "./PostSortType";
+import {JobType} from "./JobType";
+import {IntendedUse} from "./IntendedUse";
 
 /**
  * Get a list of posts.
  */
 export type GetPosts = {
-  type_?: ListingType;
+  catalog_id?: CommunityId;
+  catalog_name?: string;
   sort?: PostSortType;
-  /**
-   * Filter to within a given time range, in seconds.
-   * IE 60 would give results for the past minute.
-   * Use Zero to override the local_site and local_user time_range.
-   */
-  timeRangeSeconds?: number;
-  communityId?: CommunityId;
-  communityName?: string;
-  showHidden?: boolean;
-  /**
-   * If true, then show the read posts (even if your user setting is to hide them)
-   */
-  showRead?: boolean;
-  /**
-   * If true, then show the nsfw posts (even if your user setting is to hide them)
-   */
-  showNsfw?: boolean;
-  /**
-   * If false, then show posts with media attached (even if your user setting is to hide them)
-   */
-  hideMedia?: boolean;
-  /**
-   * Whether to automatically mark fetched posts as read.
-   */
-  markAsRead?: boolean;
-  /**
-   * If true, then only show posts with no comments
-   */
-  noCommentsOnly?: boolean;
-  pageCursor?: PaginationCursor;
-  pageBack?: boolean;
+  show_hidden?: boolean;
+  /** If true, then show the self_promotion posts (even if your user setting is to hide them) */
+  self_promotion?: boolean;
+  /** If false, then show posts with media attached (even if your user setting is to hide them) */
+  hide_media?: boolean;
+  /** If true, then only show posts with no proposals */
+  no_proposals_only?: boolean;
+  intended_use?: IntendedUse;
+  job_type?: JobType;
+  /** Minimum budget in your preferred currency */
+  budget_min?: number; // i64 in Rust maps to number (but watch for precision if > 53 bits)
+  /** Maximum budget in your preferred currency */
+  budget_max?: number;
+  requires_english?: boolean;
+  page_cursor?: PaginationCursor;
+  page_back?: boolean;
   limit?: number;
 };
