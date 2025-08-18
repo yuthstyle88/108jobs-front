bc99890f75297041cc1d7111c5e85cdeeeb05b87
koey <thekhoi.k@gmail.com>
2025-08-18 22:11:26 +0700

    remove asert

--- src/hooks/communites-api/useCommunities.ts
+++ src/hooks/communites-api/useCommunities.ts
@@ -1,10 +1,9 @@
 import {getIsoData} from "@/hooks/useIsoData";
-import {assertExists} from "@/utils/helpers";
 import {REQUEST_STATE} from "@/services/HttpService";
 
 export const useCommunities = () => {
     const isoData = getIsoData();
-    const communities = assertExists(isoData?.communities, "Missing communities list");
+    const communities = isoData?.communities;
     const state = REQUEST_STATE.SUCCESS;
     return {
         state,
