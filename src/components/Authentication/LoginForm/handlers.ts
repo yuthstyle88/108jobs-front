007a716c12b65d1ac62e6ad0e46b14cfc6b8fba3
koey <thekhoi.k@gmail.com>
2025-09-20 08:01:21 +0700

    implement change password api

--- src/components/Authentication/LoginForm/handlers.ts
+++ src/components/Authentication/LoginForm/handlers.ts
@@ -71,7 +71,7 @@ export const handleLogin = async(i: LoginFormClass, data: any) => {
           i.props.formMethods.setError("password",
             {
               type: "manual",
-              message: i.props.t("invalidPassword"),
+              message: i.props.t("error.invalidPassword"),
             });
         }
         i.setState({loginRes});
