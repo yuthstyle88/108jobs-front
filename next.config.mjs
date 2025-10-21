8e3acf2937ec04895940767f65b5743a8b8477e1
yuth <yuth_style88@hotmail.com>
2025-10-22 03:56:16 +0700

    change link to bt

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
-/** @type {import("next").NextConfig} */
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     output: "standalone",
 }
+export default nextConfig;
