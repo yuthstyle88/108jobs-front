3426f37768ff48ceea71fd43ffb018792f017954
yuth <yuth_style88@hotmail.com>
2025-10-21 23:19:27 +0700

    fix room id

--- src/app/[lang]/chat/message/[roomId]/MessageClient.tsx
+++ src/app/[lang]/chat/message/[roomId]/MessageClient.tsx
@@ -3,18 +3,16 @@
 import {useEffect, useState} from "react";
 import {PhoenixChatBridgeProvider} from "@/modules/chat/contexts/PhoenixChatBridgeProvider";
 import ChatRoomView from "../../../../../modules/chat/components/ChatRoomView";
-import {HttpService, UserService} from "@/services";
-import LoadingBlur from "@/components/Common/Loading/LoadingBlur";
-import {REQUEST_STATE} from "@/services/HttpService";
+import {UserService} from "@/services";
 import {useMyUser} from "@/hooks/profile-api/useMyUser";
 import {LocalUserId, PersonId, Post} from "@/lib/lemmy-js-client";
 import {RoomNotFound} from "@/components/RoomNotFound";
 import {useStateMachineStore} from "@/modules/chat/store/stateMachineStore";
-import {deriveAesGcmKeyHex, ensureIdentityKeyPair} from "@/utils";
 
 export default function MessageClient({roomId}: { roomId: string }) {
     const isLoggedIn = UserService.Instance.isLoggedIn;
     const {localUser} = useMyUser() as { localUser: any };
+    console.log("localUser", localUser);
     const [state, setState] = useState<{
         partnerName: string;
         partnerId?: LocalUserId;
@@ -28,37 +26,10 @@ export default function MessageClient({roomId}: { roomId: string }) {
         partnerName: "Unknown",
         notFound: false,
     });
-    const [showDelay, setShowDelay] = useState(false);
-    const isReady = Boolean(localUser && state.partnerId);
     const reset = useStateMachineStore((s) => s.reset);
     useEffect(() => {
         if (roomId) reset();
     }, [roomId, reset]);
-
-    useEffect(() => {
-        let timer: ReturnType<typeof setTimeout> | undefined;
-        if (!isReady) {
-            setShowDelay(false);
-            timer = setTimeout(() => setShowDelay(true), 300); // delay spinner 300ms
-        } else {
-            setShowDelay(false);
-        }
-        return () => {
-            if (timer) clearTimeout(timer);
-        };
-    }, [isReady]);
-
-    // if (!isReady) {
-    //     return (
-    //         <div style={{ padding: '2rem', textAlign: 'center', color: '#999' }}>
-    //             ⚠️ Chat room is not ready or missing required data.
-    //         </div>
-    //     );
-    // }
-
-    if (state.notFound) {
-        return <RoomNotFound/>;
-    }
 
     return (
         <PhoenixChatBridgeProvider
