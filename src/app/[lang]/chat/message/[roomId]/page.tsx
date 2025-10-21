4aa7847dd9442bbd8ee9656164e001aef7480df1
yuth <yuth_style88@hotmail.com>
2025-10-21 22:10:10 +0700

    fix room id

--- src/app/[lang]/chat/message/[roomId]/page.tsx
+++ src/app/[lang]/chat/message/[roomId]/page.tsx
@@ -5,7 +5,8 @@ export default async function ChatMessage({
 }: {
   params: Promise<{roomId: string}>;
 }) {
-  const {roomId} = await params;
+  // const {roomId} = await params;
+  const roomId = "fd6b6372a24778a3";
   console.log('ChatMessage params:', params); // ดูใน server log
   return (
     <MessageClient roomId={roomId}/>
