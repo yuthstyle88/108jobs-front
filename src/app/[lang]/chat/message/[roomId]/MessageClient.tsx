b02f0bdd8de8debfd0293f0b775e6098e079e61e
yuth <yuth_style88@hotmail.com>
2025-10-21 22:12:49 +0700

    fix room id

--- src/app/[lang]/chat/message/[roomId]/MessageClient.tsx
+++ src/app/[lang]/chat/message/[roomId]/MessageClient.tsx
@@ -48,13 +48,13 @@ export default function MessageClient({roomId}: { roomId: string }) {
         };
     }, [isReady]);
 
-    if (!isReady) {
-        return (
-            <div style={{ padding: '2rem', textAlign: 'center', color: '#999' }}>
-                ⚠️ Chat room is not ready or missing required data.
-            </div>
-        );
-    }
+    // if (!isReady) {
+    //     return (
+    //         <div style={{ padding: '2rem', textAlign: 'center', color: '#999' }}>
+    //             ⚠️ Chat room is not ready or missing required data.
+    //         </div>
+    //     );
+    // }
 
     if (state.notFound) {
         return <RoomNotFound/>;
