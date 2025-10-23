3d567f1cc69d437ec222c531128ab9951de24dbc
yuth <yuth_style88@hotmail.com>
2025-10-23 10:23:18 +0700

    optimize image

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,7 @@ const nextConfig = {
     eslint: { ignoreDuringBuilds: true },
     typescript: { ignoreBuildErrors: true },
     images: {
-        formats: ["image/avif", "image/webp"],
-        minimumCacheTTL: 60 * 60 * 24 * 30, // 30 days
-        // Allow serving local SVGs via next/image (not optimized) safely
-        dangerouslyAllowSVG: true,
-        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-        deviceSizes: [320, 420, 768, 1024, 1280, 1536, 1920],
-        imageSizes: [16, 24, 32, 48, 64, 96, 128, 256, 384],
+        unoptimized: true,
     },
 }
 export default nextConfig;
